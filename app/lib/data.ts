b985e8f42566a578d9998e021cb4fd1419627ae9
YAN <yanyuanfeng_sspu@163.com>
2026-05-14 17:51:50 +0800

    feat: 添加发票页面、加载组件及数据获取功能

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -19,8 +19,8 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
@@ -61,6 +61,7 @@ export async function fetchLatestInvoices() {
  */
 export async function fetchCardData() {
   try {
+    //  await new Promise((resolve) => setTimeout(resolve, 3000));
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
